b8da063e4053f62889e3a6107be2473237a66e38
jackming123 <1210704663@qq.com>
2024-06-25 03:42:19 +0000

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -8,6 +8,7 @@ use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+use Illuminate\Support\Facades\DB;
 
 class TopicObserver
 {
@@ -35,6 +36,11 @@ class TopicObserver
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
         }
+    }
+
+    public function deleted(Topic $topic)
+    {
+        DB::table('replies')->where('topic_id', $topic->id)->delete();
     }
 
 }
